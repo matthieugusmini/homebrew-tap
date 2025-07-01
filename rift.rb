966e3c7fbc41103634a4a3523e3768c43267a304
Matthieu Gusmini <simfy2602@outlook.fr>
2025-07-01 18:31:52 +0200

    Brew formula update for rift version v0.1.1

--- rift.rb
+++ rift.rb
@@ -5,21 +5,21 @@
 class Rift < Formula
   desc "LoL Esports in your terminal"
   homepage "https://github.com/matthieugusmini/rift"
-  version "0.1.0"
+  version "0.1.1"
   license "mit"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.0/rift_Darwin_x86_64.tar.gz"
-      sha256 "5963e451f8f1293b752580831b3f3c1fc68903613cc03ebaeb2197f81e1c9392"
+      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.1/rift_Darwin_x86_64.tar.gz"
+      sha256 "5b0277ab0306b272b2b2bcf9af596907023114ef3cd800c1534b5f6f4a9ef7c8"
 
       def install
         bin.install "rift"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.0/rift_Darwin_arm64.tar.gz"
-      sha256 "9196e41597ca4f7ce2df4f75806ca11671bba6b4b6efa6d88dcf9b4a38fe44a6"
+      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.1/rift_Darwin_arm64.tar.gz"
+      sha256 "e406ae4df250bc3f0fe2c78be848fae728433616fc52f6dd2f2dc97479d44407"
 
       def install
         bin.install "rift"
@@ -29,15 +29,15 @@ class Rift < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.0/rift_Linux_x86_64.tar.gz"
-      sha256 "1b54bcdfb512e47163607261f629f6b5853e77c73914c4c32cbdabc6b69f7590"
+      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.1/rift_Linux_x86_64.tar.gz"
+      sha256 "c4d03e35b800930109a1e595d3dcd0e4d9bed709c9e63eb660508c57d6ea1c2b"
       def install
         bin.install "rift"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.0/rift_Linux_arm64.tar.gz"
-      sha256 "5584079cb9b9861dd32597c8c3951cffdb950f87ace45b86c9d7e27daebd3a1c"
+      url "https://github.com/matthieugusmini/rift/releases/download/v0.1.1/rift_Linux_arm64.tar.gz"
+      sha256 "23e6c1c521138331fcd59dcf295b3eca6ad99104b4bc3915e54400d7b81f9804"
       def install
         bin.install "rift"
       end
